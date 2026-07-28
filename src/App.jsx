fb8b0208584dfe6d84608058b80062eefcd49440
BTC001B <btc001b.btcglobal@gmail.com>
2026-07-28 13:55:10 +0530

    small correction

--- src/App.jsx
+++ src/App.jsx
@@ -333,8 +333,7 @@ function App() {
                     </div>
 
                     <div className="info-box-light" style={{ marginTop: '24px' }}>
-                      <Info size={18} />
-                      <p>To add a new email address, you must register it through the BNX Mail application.</p>
+                      <Info size={18} /><p>To add a new email address, you must register it through the BNX Mail application.</p>
                     </div>
                   </div>
                 </motion.div>
