c6a9b8f04f42a6455c53b70a9cf5d2adf12b2dcd
BTC001B <btc001b.btcglobal@gmail.com>
2026-08-22 10:42:33 +0530

    fix: correct localStorage key reference from variable to string literal in App component

--- src/App.jsx
+++ src/App.jsx
@@ -668,7 +668,7 @@ function App() {
                   animate={{ opacity: 1, y: 0 }}
                   exit={{ opacity: 0, y: -10 }}
                 >
-                  <SubIdManager token={localStorage.getItem(bnx_accessToken)} user={user} />
+                  <SubIdManager token={localStorage.getItem('bnx_accessToken')} user={user} />
                 </motion.div>
               )}
             </AnimatePresence>
